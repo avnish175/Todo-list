5e2e72e9af241a00e1ae2d17822b2a3de2d26162
avnish175 <125601430+avnish175@users.noreply.github.com>
2023-11-30 21:41:29 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+
 
 function App() {
+const [task,setTask]=useState([]);
+const [newTask,setNewTask]=useState("");
+
+const handleChange=(event)=>{
+  const addTask=event.target.value;
+  setNewTask(addTask);
+};
+const handleClick=()=>{
+  const abtask={
+    id:task.length===0? 1:task[task.length-1].id+1,
+    taskName:newTask,
+    complete:false
+  }
+  const newTodo=[...task,abtask];
+  setTask(newTodo);
+}
+const deleteTask=(id)=>{
+  setTask(task.filter((one)=> id!==one.id));  //task array se ek ek kar match karega
+}
+const completeTase=(id)=>{
+  setTask(task.map((one)=>{
+    if(one.id===id){
+      return{...one,complete:true};
+    }
+    else{
+      return one;
+    }
+  }
+  ));
+
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='addTask'>
+      <input onChange={handleChange}/>
+      <button onClick={handleClick}>Add task</button>
+      </div>
+      <div className="list">
+        {task.map((task)=>{
+         return <div>
+          <h1 style={{background:task.complete===true?"green": "blue"}}>{task.taskName}</h1> 
+          <button onClick={()=>deleteTask(task.id)}>X</button>
+          <button onClick={()=>completeTase(task.id)}>complete</button>
+          </div>}
+        )}
+      </div>
     </div>
   );
 }
